ec08368d47eea28a1e70652192c76efa08aeb7ce
Pauline OLLIVEAUD <pauline.olliveaud2019@campus-eni.fr>
2020-02-25 14:21:18 +0100

    remove old version, add new one perso

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Parallax, ParallaxLayer } from 'react-spring/renderprops-addons'
+import './index.css'
 
 // Little helpers ...
 const url = (name, wrap = false) => `${wrap ? 'url(' : ''}https://awv3node-homepage.surge.sh/build/assets/${name}.svg${wrap ? ')' : ''}`
@@ -15,31 +16,36 @@ class App extends React.Component {
   render() {
     return (
       <Parallax ref={ref => (this.parallax = ref)} pages={3}>
+
         <ParallaxLayer offset={1} speed={1} style={{ backgroundColor: '#805E73' }} />
         <ParallaxLayer offset={2} speed={1} style={{ backgroundColor: '#87BCDE' }} />
+
 
         <ParallaxLayer offset={0} speed={0} factor={3} style={{ backgroundImage: url('stars', true), backgroundSize: 'cover' }} />
 
         <ParallaxLayer offset={1.3} speed={-0.3} style={{ pointerEvents: 'none' }}>
-          <img src={url('satellite4')} style={{ width: '15%', marginLeft: '70%' }} />
+          <img src="./origamibirds.png"style={{ width: '15%', marginLeft: '70%' }} />
         </ParallaxLayer>
 
-        <ParallaxLayer offset={1} speed={0.8} style={{ opacity: 0.1 }}>
+        <ParallaxLayer offset={0} speed={0} factor={3} style={{ backgroundImage: url('stars', true), backgroundSize: 'cover' }} />
+
+
+        {/* <ParallaxLayer offset={1} speed={0.8} style={{ opacity: 0.1 }}>
           <img src={url('cloud')} style={{ display: 'block', width: '20%', marginLeft: '55%' }} />
           <img src={url('cloud')} style={{ display: 'block', width: '10%', marginLeft: '15%' }} />
-        </ParallaxLayer>
+        </ParallaxLayer> */}
 
-        <ParallaxLayer offset={1.75} speed={0.5} style={{ opacity: 0.1 }}>
+        {/* <ParallaxLayer offset={1.75} speed={0.5} style={{ opacity: 0.1 }}>
           <img src={url('cloud')} style={{ display: 'block', width: '20%', marginLeft: '70%' }} />
           <img src={url('cloud')} style={{ display: 'block', width: '20%', marginLeft: '40%' }} />
-        </ParallaxLayer>
+        </ParallaxLayer> */}
 
-        <ParallaxLayer offset={1} speed={0.2} style={{ opacity: 0.2 }}>
+        {/* <ParallaxLayer offset={1} speed={0.2} style={{ opacity: 0.2 }}>
           <img src={url('cloud')} style={{ display: 'block', width: '10%', marginLeft: '10%' }} />
           <img src={url('cloud')} style={{ display: 'block', width: '20%', marginLeft: '75%' }} />
-        </ParallaxLayer>
+        </ParallaxLayer> */}
 
-        <ParallaxLayer offset={1.6} speed={-0.1} style={{ opacity: 0.4 }}>
+        {/* <ParallaxLayer offset={1.6} speed={-0.1} style={{ opacity: 0.4 }}>
           <img src={url('cloud')} style={{ display: 'block', width: '20%', marginLeft: '60%' }} />
           <img src={url('cloud')} style={{ display: 'block', width: '25%', marginLeft: '30%' }} />
           <img src={url('cloud')} style={{ display: 'block', width: '10%', marginLeft: '80%' }} />
@@ -48,13 +54,13 @@ class App extends React.Component {
         <ParallaxLayer offset={2.6} speed={0.4} style={{ opacity: 0.6 }}>
           <img src={url('cloud')} style={{ display: 'block', width: '20%', marginLeft: '5%' }} />
           <img src={url('cloud')} style={{ display: 'block', width: '15%', marginLeft: '75%' }} />
-        </ParallaxLayer>
+        </ParallaxLayer> */}
 
-        <ParallaxLayer offset={2.5} speed={-0.4} style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', pointerEvents: 'none' }}>
+        {/* <ParallaxLayer offset={2.5} speed={-0.4} style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', pointerEvents: 'none' }}>
           <img src={url('earth')} style={{ width: '60%' }} />
-        </ParallaxLayer>
+        </ParallaxLayer> */}
 
-        <ParallaxLayer
+        {/* <ParallaxLayer
           offset={2}
           speed={-0.3}
           style={{
@@ -62,14 +68,22 @@ class App extends React.Component {
             backgroundPosition: 'center',
             backgroundImage: url('clients', true)
           }}
-        />
+        /> */}
 
         <ParallaxLayer
           offset={0}
           speed={0.1}
           onClick={() => this.parallax.scrollTo(1)}
           style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-          <img src={url('server')} style={{ width: '20%' }} />
+          {
+              <div class="scroll">
+                  <p>SCROLL</p>
+                    <svg version="1.1" x="0px" y="0px" viewBox="0 0 167 299">
+                        <polygon class="fill-3" points="167,73 83.5,298.9 0,73 " style={{ fill:"pink"}}></polygon>
+                        <polygon class="fill-1" points="137.4,0 83.5,145.9 29.6,0 " style={{ fill:"#805E73"}} ></polygon>
+                    </svg>
+             </div>
+          }
         </ParallaxLayer>
 
         <ParallaxLayer
@@ -77,7 +91,12 @@ class App extends React.Component {
           speed={0.1}
           onClick={() => this.parallax.scrollTo(2)}
           style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-          <img src={url('bash')} style={{ width: '40%' }} />
+              {
+                  <div class="intro">
+                      <h2> Portrait</h2>
+                  </div>
+              }
+          
         </ParallaxLayer>
 
         <ParallaxLayer
@@ -85,7 +104,12 @@ class App extends React.Component {
           speed={-0}
           style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}
           onClick={() => this.parallax.scrollTo(0)}>
-          <img src={url('clients-main')} style={{ width: '40%' }} />
+               {
+                  <div class="intro">
+                      <h2> CV</h2>
+                  </div>
+              }
+         
         </ParallaxLayer>
       </Parallax>
     )
